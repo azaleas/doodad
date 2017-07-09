871a789ca21733f293c1f499ee91431f0441158c
azaleas <azaleas@2die4.com>
2017-07-09 15:31:18 +0400

    added fetch for rooms

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,15 @@
 const API_STEM = 'https://5961ca8d8492d90011f12d01.mockapi.io/doodad';
 
 const api = {
-    
+    fetchRoomsData(){
+        let URL = `${API_STEM}/rooms/`;
+        return fetch(URL)
+            .then((response) => response.json())
+            .then((responseJSON) => responseJSON)
+            .catch(function(err){
+                console.warn("Error in fetchRoomsData", err);
+            })
+    }
 };
 
 export default api;
