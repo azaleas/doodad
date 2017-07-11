32f0e37a85b18508e17d7eee2d8de6726b352527
azaleas <azaleas@2die4.com>
2017-07-11 13:25:06 +0400

    added menu

--- src/components/HeaderTitleComponent.js
+++ src/components/HeaderTitleComponent.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'react-router-dom/Link';
+
+import { Dropdown, Icon, Menu } from 'semantic-ui-react'
+
 
 const HeaderTitleComponent = (props) => {
     const pathname = window.location.pathname;
     return (
-        <div>
-            <h1 className="app--title tac">- doodad -</h1>
-            <p className="app--desc tac">Control Panel Demo</p>
+        <div className="ui container">
+            <Menu attached='top'>
+                <Dropdown item icon='home' simple>
+                    <Dropdown.Menu>
+                        <Link to="/">
+                            <Dropdown.Item className="tac">
+                                Main
+                            </Dropdown.Item>
+                        </Link>
+                        <Link to="/home">
+                            <Dropdown.Item className="tac">
+                                Home
+                            </Dropdown.Item>
+                        </Link>
+                        <Link to="/rooms">
+                            <Dropdown.Item className="tac">
+                                Rooms
+                            </Dropdown.Item>
+                        </Link>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Menu.Menu position='right'>
+                    <div className='ui right aligned category item'>
+                        <div className='ui transparent icon input'>
+                            <h1 className="app--title">doodad / <span className="app--desc">Control Panel Demo</span></h1>
+                        </div>
+                    </div>
+                </Menu.Menu>
+            </Menu>
+            <br/>
         </div>
     );
 };
