bc13b36880759604328f3828dccd8ebfdd97944d
azaleas <azaleas@2die4.com>
2017-07-11 13:26:22 +0400

    added buttons for creation of new rooms and appliances

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 
 import CreateRoomContainer from './../containers/CreateRoomContainer';
 import CreateHomeApplianceContainer from './../containers/CreateHomeApplianceContainer';
+import CreateRoomApplianceContainer from './../containers/CreateRoomApplianceContainer';
 import HeaderTitleComponent from './HeaderTitleComponent';
 
 const Header = (props) => {
     const pathname = window.location.pathname;
-    if(pathname === "/" || pathname === "/home"){
+    const roomsRegExp = /\/rooms\/(\d+)$/;
+    if((pathname.match(/\/$/) !== null && pathname.match(/\/$/)[0])
+        || (pathname.match(/\/home$/) && pathname.match(/\/home$/)[0]) 
+        || (pathname.match(/\/rooms$/) !== null && pathname.match(/\/rooms$/)[0])){
         return (
             <header>
                 <HeaderTitleComponent/>
@@ -17,6 +21,25 @@ const Header = (props) => {
                 </div>
             </header>
         );
+    }
+    else if(pathname.match(roomsRegExp) !== null && pathname.match(roomsRegExp)[0]){
+        return (
+            <header>
+                <HeaderTitleComponent/>
+                <div className="ui centered two column stackable grid container">
+                    <CreateHomeApplianceContainer/>
+                    <CreateRoomContainer/>
+                    <CreateRoomApplianceContainer roomId={Number(pathname.match(roomsRegExp)[1])}/>
+                </div>
+            </header>
+        );
+    }
+    else{
+        return(
+            <header>
+                <HeaderTitleComponent/>
+            </header>
+        )
     }
 };
 
