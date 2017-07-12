2689316b658cc1c370973e67014ea9b7d63addec
azaleas <azaleas@2die4.com>
2017-07-12 10:37:08 +0400

    Changed MainContainer to a stateless Component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Link from 'react-router-dom/Link';
+
+/*
+    Main Screen. Will have Home And Rooms Link. 
+*/
+
+const MainComponent = (props) => {
+    return (
+        <div className="ui centered two column stackable grid container">
+            <div className="column">
+                <Link 
+                    className="baseblock tac"
+                    to="home">
+                    <h1 className="baseblock--title">Home</h1>
+                </Link>
+            </div>
+            <div className="column">
+                <Link 
+                    className="baseblock tac"
+                    to="rooms">
+                    <h1 className="baseblock--title">Rooms</h1>
+                </Link>
+            </div>
+        </div> 
+    );
+};
+
+MainComponent.propTypes = {
+    className: PropTypes.string,
+};
+
+export default MainComponent;
