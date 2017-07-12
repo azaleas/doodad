042799ba28c49e5d581b3186d0dbe0716ab5b1ca
azaleas <azaleas@2die4.com>
2017-07-12 17:29:31 +0400

    added rooms container test file

--- src/tests/containers/RoomsContainer.test.js
+++ src/tests/containers/RoomsContainer.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import RoomsContainer from './../../containers/RoomsContainer';
+
+import api from './../../utils/api';
+
+import mockData from './../mockData';
+
+const mock_roomsdata = mockData.roomsData;
+
+jest.mock('./../../utils/api', () => ({
+    fetchRoomsData: jest.fn(() => Promise.resolve(mock_roomsdata)),
+}));
 
 describe('RoomsContainer', () => {
     let wrapper;
@@ -17,4 +27,37 @@ describe('RoomsContainer', () => {
     it('renders without crashing', () => {
         wrapper;
     });
+
+    it('should redirect if `redirect` state property is true', () => {
+        wrapper.setState({redirect: true});
+        expect(
+            wrapper.find('Redirect').exists()
+        ).toBe(true);
+    });
+    
+    it('should have `RoomsComponent` \
+        when data is fetched and `redirect` state is false', () => {
+        wrapper.setState({redirect: false});
+        expect(
+            wrapper.find('RoomsComponent').exists()
+        ).toBe(true);
+    });
+
+    it('should receive rooms data from server \
+        and update the `roomsData` state', ()=> {
+        /*
+            api fetching happens on componentDidMount, 
+            for which enzyme mount has to be used. 
+            Ignore warnings about Failed context type.
+         */
+        let wrapper = mount(<RoomsContainer location={pathname}/>);
+        return api.fetchRoomsData()
+                .then((response) => {
+                    expect(response).toEqual(mock_roomsdata);
+                    expect(
+                        wrapper.state().roomsData
+                    ).toEqual(mock_roomsdata);
+                })
+    });
+
 })  
